b74fdcf9e46bcb61e81b98b95572451df1191348
satyam kale <satyamkale271@gmail.com>
2024-02-11 22:56:33 +0530

    made profile card with react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const profiledata = [
+  {
+    name: "SATYAM KALE",
+    bio: "open source enthusiast, love to contribute and currently learning reverse engineering and binary exploitation.",
+    photoName: "images/satyam.jpg",
+  },
+];
+
+function App() {
+  return (
+    <div>
+      <Body />
+    </div>
+  );
+}
+
+function Body() {
+  const profile = profiledata;
+  return (
+    <div className="center">
+      {profile.map((profiles) => (
+        <Card cardobj={profiles} key={profiles.name} />
+      ))}
+    </div>
+  );
+}
+
+function Card({ cardobj }) {
+  return (
+    <div className="color">
+      <img src={cardobj.photoName} alt={cardobj.name} />
+      <h1>{cardobj.name}</h1>
+      <p>{cardobj.bio}</p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
